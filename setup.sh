78d22ad697328a9c7163c9f9c1715146b2652a9c
takashi1coq <maedaw1nb@gmail.com>
2020-10-01 19:10:21 +0900

    add mkdir work/compile

--- setup.sh
+++ setup.sh
@@ -19,6 +19,7 @@ function GitEmail() {
 DOT_FILES=( .config/nvim .gitconfig .gitignore_global .ctags .myshrc )
 mkdir -p $HOME/.config
 mkdir -p $HOME/work/src
+mkdir -p $HOME/work/compile
 for file in ${DOT_FILES[@]}
 do
   # -fnは上書きオプション
