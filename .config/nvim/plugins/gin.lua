f110f36a4839401085c04284f8c8dbe5a82ba6a6
takashi1coq <maedaw1nb@gmail.com>
2023-06-04 12:01:42 +0900

    improve gin

--- .config/nvim/plugins/gin.lua
+++ .config/nvim/plugins/gin.lua
@@ -1,63 +1,7 @@
---[[==========================================================
- remapping gin command
-============================================================]]
--- status
-vim.g.gin_status_default_args = {
-  '++opener=tabnew'
-}
-vim.keymap.set('n', '<Space>l', function () vim.cmd('GinStatus') end)
--- log
-vim.g.gin_log_default_args = {
-  '++opener=tabnew'
-  , '--graph'
-  , '--decorate=full'
-}
-vim.api.nvim_create_user_command(
-  'LogBranch'
-  , function () vim.cmd('GinLog '..vim.fn['gin#component#branch#unicode']()) end
-  , { nargs = 0 }
-)
-vim.api.nvim_create_user_command(
-  'LogCurrent'
-  -- TODO ....?
-  , function () vim.cmd('GinBuffer ++opener=tabnew log %') end
-  , { nargs = 0 }
-)
-
--- blame
-vim.api.nvim_create_user_command(
-  'Blame'
-  , function ()
-    vim.cmd('GinBuffer ++opener=vsplit blame -M -n %')
-    vim.wo.wrap = false
-    vim.bo.filetype = 'my-gin-blame'
-  end
-  , { nargs = 0 }
-)
-
--- branch
-vim.g.gin_branch_default_args = {
-  '++opener=tabnew'
-}
-vim.api.nvim_create_user_command(
-  'BranchAll'
-  , function () vim.cmd('GinBranch -a') end
-  , { nargs = 0 }
-)
-vim.keymap.set('n', '<Space>k', function () vim.cmd('BranchAll') end)
-
--- stash
-vim.api.nvim_create_user_command('StashSave', function () vim.cmd('Gin stash save -u "gina stash"') end, { nargs = 0 })
-vim.api.nvim_create_user_command('StashPop', function () vim.cmd('Gin stash pop') end, { nargs = 0 })
-vim.api.nvim_create_user_command('StashList', function () vim.cmd('Gin stash list') end, { nargs = 0 })
-vim.api.nvim_create_user_command('StashClear', function () vim.cmd('Gin stash clear') end, { nargs = 0 })
-
---[[==========================================================
- filyType setup
-============================================================]]
 local fileType = {}
 
 -- status
+vim.keymap.set('n', '<Space>l', function () vim.cmd('GinStatus ++opener=tabnew') end)
 vim.g.gin_status_disable_default_mappings = true
 fileType['gin-status'] = function ()
   VimBufferKeymapSet('n', 'dd'
@@ -69,7 +13,7 @@ fileType['gin-status'] = function ()
     end
   )
   VimBufferKeymapSet({'n','v'}, 'cc', '<Plug>(gin-action-patch:worktree)')
-  VimBufferKeymapSet({'n','v'}, '<CR>', '<Plug>(gin-action-edit:local:tabedit)')
+  VimBufferKeymapSet({'n','v'}, '<CR>', '<Plug>(gin-action-edit:local)')
   VimBufferKeymapSet({'n','v'}, '<<', '<Plug>(gin-action-stage)')
   VimBufferKeymapSet({'n','v'}, '==', '<Plug>(gin-action-stash)')
   VimBufferKeymapSet({'n','v'}, '>>', '<Plug>(gin-action-unstage)')
@@ -86,27 +30,36 @@ fileType['gin-status'] = function ()
 end
 
 -- log
+vim.g.gin_log_default_args = {
+  '++opener=tabnew'
+  , '--graph'
+  , '--decorate=full'
+}
+vim.api.nvim_create_user_command(
+  'LogBranch'
+  , function () vim.cmd('GinLog '..vim.fn['gin#component#branch#unicode']()) end
+  , { nargs = 0 }
+)
+vim.api.nvim_create_user_command(
+  'LogCurrent'
+  , function ()
+    vim.cmd('GinLog -- %')
+  end
+  , { nargs = 0 }
+)
 vim.g.gin_log_disable_default_mappings = true
 fileType['gin-log'] = function ()
-  vim.api.nvim_create_user_command(
-    'ChangesCommit'
-    , function ()
-    end
-    , { nargs = 0 }
-  )
   VimBufferKeymapSet('n', 'a', '<Plug>(gin-action-choice)')
   VimBufferKeymapSet('n', 'yy', '<Plug>(gin-action-yank:commit)')
   VimBufferKeymapSet('n', 'ch', function ()
     vim.cmd([[execute "normal \<Plug>(gin-action-yank:commit)"]])
     local commit = vim.fn.getreg('+')
-    vim.cmd('GinBuffer ++opener=tabnew diff '..commit..'^ '..commit..' --name-only')
-    vim.bo.filetype = 'gin-my-changes'
+    vim.cmd('GinBuffer ++opener=split diff '..commit..'^ '..commit..' --name-only')
+    vim.bo.filetype = 'gin-my-log-changes'
     vim.g.changes_git_commit = commit
   end)
 end
-
--- my-changes
-fileType['gin-my-changes'] = function ()
+fileType['gin-my-log-changes'] = function ()
   VimBufferKeymapSet('n', 'cc', function ()
     local path = vim.fn.getline('.')
     vim.cmd('GinEdit ++opener=tabnew '..vim.g.changes_git_commit..'^ '..path..'|diffthis')
@@ -115,15 +68,26 @@ fileType['gin-my-changes'] = function ()
     vim.bo.filetype = 'gin-my-patch'
   end)
   VimBufferKeymapSet('n', '<CR>', function ()
+    -- TODO first commit can not see..?
     local path = vim.fn.getline('.')
-    vim.cmd('tabnew '..path)
+    vim.cmd('GinEdit ++opener=tabnew '..vim.g.changes_git_commit..'^ '..path)
   end)
 end
 
--- my-blame
+-- blame
+vim.api.nvim_create_user_command(
+  'Blame'
+  , function ()
+    vim.cmd('GinBuffer ++opener=vsplit blame -M -n %')
+    vim.wo.wrap = false
+    vim.bo.filetype = 'my-gin-blame'
+  end
+  , { nargs = 0 }
+)
 -- TODO show commit message..
 
 -- branch
+vim.keymap.set('n', '<Space>k', function () vim.cmd('GinBranch ++opener=tabnew -a') end)
 vim.g.gin_branch_disable_default_mappings = true
 fileType['gin-branch'] = function ()
   VimBufferKeymapSet('n', '<CR>', '<Plug>(gin-action-switch)')
@@ -132,6 +96,12 @@ fileType['gin-branch'] = function ()
   VimBufferKeymapSet({'n','v'}, 'a', '<Plug>(gin-action-choice)')
   VimBufferKeymapSet({'n','v'}, 'dd', '<Plug>(gin-action-delete)')
 end
+
+-- stash
+vim.api.nvim_create_user_command('StashSave', function () vim.cmd('Gin stash save -u "gina stash"') end, { nargs = 0 })
+vim.api.nvim_create_user_command('StashPop', function () vim.cmd('Gin stash pop') end, { nargs = 0 })
+vim.api.nvim_create_user_command('StashList', function () vim.cmd('Gin stash list') end, { nargs = 0 })
+vim.api.nvim_create_user_command('StashClear', function () vim.cmd('Gin stash clear') end, { nargs = 0 })
 
 -- set keymap
 SetFileTypeKeyMap(fileType, 'gin_augroup')
