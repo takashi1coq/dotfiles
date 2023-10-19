298df8e607cd1cf445ff6eb7a2c54189321c8af8
takashi1coq <maedaw1nb@gmail.com>
2023-10-19 20:13:46 +0900

    improve

--- .config/nvim/plugins/mr.lua
+++ .config/nvim/plugins/mr.lua
@@ -2,7 +2,7 @@ vim.g['mr#threshold'] = 50
 vim.g['mr#mrw#predicates'] = {
   function(filename)
     return vim.regex([[COMMIT_EDITMSG]]):match_str(filename) == nil
-  end,
+  end
 }
 vim.api.nvim_create_user_command(
   'OpenMrwFile'
