a7ce80e0e7508ca666cf51fd266d33cb6dc41706
takashi1coq <takashi@gmail.com>
2025-10-22 17:23:54 +0900

    disabled merkdown from mrw

--- nvim/lua/plugins/mr/setting.lua
+++ nvim/lua/plugins/mr/setting.lua
@@ -1,7 +1,11 @@
 
 vim.g['mr#threshold'] = 50
 vim.g['mr#mrw#predicates'] = {
-  function(filename)
+  function (filename)
     return vim.regex([[COMMIT_EDITMSG]]):match_str(filename) == nil
+  end
+  , function (filename)
+    local bufnr = vim.fn.bufnr(filename)
+    return bufnr ~= -1 and vim.bo[bufnr].filetype ~= "markdown"
   end
 }
